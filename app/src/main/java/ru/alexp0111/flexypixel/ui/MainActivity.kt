51de2a6c02799e4d06ef87184fb3a17cdfed53c4
Bulat <konnor1500@icloud.com>
2024-05-10 01:40:57 +0300

    Added drag and drop to DisplayLevelFragment.kt

--- app/src/main/java/ru/alexp0111/flexypixel/ui/MainActivity.kt
+++ app/src/main/java/ru/alexp0111/flexypixel/ui/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity() {
         injectSelf()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        router.replaceScreen(Screens.StartScreen())
+        router.replaceScreen(Screens.DisplayLevelScreen())
     }
 
     override fun onResume() {
